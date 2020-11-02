469656200437c3bb211a941f494875e2018b7572
RenHec <31858903+RenHec@users.noreply.github.com>
2020-11-01 21:27:02 -0600

    Pantall

--- resources/views/vehiculo.blade.php
+++ resources/views/vehiculo.blade.php
@@ -18,10 +18,10 @@
     <div class="row" style="background-color: #d0d0d0;"> 
         <div class="col-sm-12 col-md-6 col-lg-6">
             <div class="row">
-                <div class="col-sm-12 col-md-12 col-lg-12">
+                <div class="col-md-12 col-lg-12">
                     <p style="color: #808080;"><strong style="font-size: 2vw;">id: </strong><span style="font-size: 3vw;">{{ $vehiculo->codigo }}</span></p>
                 </div>      
-                <div class="col-sm-12 col-md-12 col-lg-12">
+                <div class="col-md-12 col-lg-12">
                     <div id="gallery-lightbox" class="row" data-toggle="modal" data-target="#exampleModal">
                         @foreach ($images as $key => $item)
                         <div class="col-md-3 col-lg-3 p-0">
