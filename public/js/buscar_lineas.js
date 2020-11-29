0491548c4be462055fc5c9db545c906a6bd7e48d
RenHec <31858903+RenHec@users.noreply.github.com>
2020-11-29 17:46:48 -0600

    Cambios

--- public/js/buscar_lineas.js
+++ public/js/buscar_lineas.js
@@ -1,25 +1,23 @@
-$(document).ready(function () {
-    $("#marca_id").change(function () {
-        var url = $(this).val();
+$(document).on('change', '#marca_id', function (e) {
+    var url = $(this).val();
 
-        if (url) {
-            $('.preloader').show()
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'json',
-                success: function (r) {
-                    var producto_select = '<option value="">Seleccione línea</option>'
-                    r.forEach(element => {
-                        producto_select += '<option value="' + element.id + '">' + element.name + '</option>';
-                    });                      
+    if (url) {
+        $('.preloader').show()
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'json',
+            success: function (r) {
+                var producto_select = '<option value="">Seleccione línea</option>'
+                r.forEach(element => {
+                    producto_select += '<option value="' + element.id + '">' + element.name + '</option>';
+                });
 
-                    $("#linea_id").html(producto_select);
-                    $('.preloader').hide()
-                }
-            })
-        } else {
-            $('.preloader').hide()
-        }
-    });
-});
+                $("#linea_id").html(producto_select);
+                $('.preloader').hide()
+            }
+        })
+    } else {
+        $('.preloader').hide()
+    }
+})
